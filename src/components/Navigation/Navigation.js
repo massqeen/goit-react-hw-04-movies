ef3d0e722815e7f487bebb5024d91698a2a6603e
massqeen <62159695+massqeen@users.noreply.github.com>
2020-10-11 00:01:55 +0300

    Added styles for the nav bar

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,18 +1,43 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { Button } from '@material-ui/core';
+import Divider from '@material-ui/core/Divider';
 import PropTypes from 'prop-types';
 
+const style = { textDecoration: 'none', fontSize: '16px', color: '#3f51b5' };
 const Navigation = () => (
-  <ul>
-    <li>
-      <NavLink to="/" exact>
-        Home
-      </NavLink>
-    </li>
-    <li>
-      <NavLink to="/movies">Movies</NavLink>
-    </li>
-  </ul>
+  <>
+    <ul style={{ display: 'flex' }}>
+      <li>
+        <Button
+          variant="outlined"
+          color="default"
+          style={{ marginRight: '10px', backgroundColor: '#f0f0f0' }}
+        >
+          <NavLink style={style} to="/" exact activeStyle={{ color: 'red' }}>
+            Home
+          </NavLink>
+        </Button>
+      </li>
+      <li>
+        <Button
+          variant="outlined"
+          color="primary"
+          style={{ backgroundColor: '#f0f0f0' }}
+        >
+          <NavLink
+            to="/movies"
+            exact
+            style={style}
+            activeStyle={{ color: 'red' }}
+          >
+            Movies
+          </NavLink>
+        </Button>
+      </li>
+    </ul>
+    <Divider style={{ margin: '16px' }} />
+  </>
 );
 
 export default Navigation;
