852b50b1c65a26d1823222de0251c25916cc8645
massqeen <62159695+massqeen@users.noreply.github.com>
2020-12-25 00:04:03 +0200

    Added code splitting - route chunks

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
-import React, { Component } from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Container from './components/Container/Container';
-import HomeView from './views/Home/HomeView';
-import MoviesView from './views/Movies/MoviesView';
-import MovieDetailsView from './views/MovieDetails/MovieDetailsView';
 import Navigation from './components/Navigation/Navigation';
+import Spinner from './components/Spinner';
 
-class App extends Component {
-  state = {};
+const HomeView = lazy(
+  () => import('./views/Home/HomeView') /* webpackChunkName: "homeView" */
+);
+const MoviesView = lazy(
+  () => import('./views/Movies/MoviesView') /* webpackChunkName: "moviesView" */
+);
+const MovieDetailsView = lazy(
+  () =>
+    import(
+      './views/MovieDetails/MovieDetailsView'
+    ) /* webpackChunkName: "movieDetailsView" */
+);
 
-  render() {
-    return (
-      <Container>
-        <Navigation />
-        <Switch>
-          <Route path="/" exact component={HomeView} />
-          <Route path="/movies" exact component={MoviesView} />
-          <Route path="/movies/:movieId" component={MovieDetailsView} />
-          <Redirect to="/" />
-        </Switch>
-      </Container>
-    );
-  }
-}
+const App = () => (
+  <Container>
+    <Navigation />
+    <Suspense fallback={<Spinner />}>
+      <Switch>
+        <Route path="/" exact component={HomeView} />
+        <Route path="/movies" exact component={MoviesView} />
+        <Route path="/movies/:movieId" component={MovieDetailsView} />
+        <Redirect to="/" />
+      </Switch>
+    </Suspense>
+  </Container>
+);
 
 export default App;
